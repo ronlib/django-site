d2e1f4bf5f3d567710dad1ce12d02d393aa2a823
Ron <ronll64@gmail.com>
2015-12-26 13:52:52 +0200

    Added "submitter" attribute to Question

--- polls/models.py
+++ polls/models.py
@@ -1,22 +1,17 @@
 import datetime
 from django.contrib import auth
 from django.db import models
-from django.utils import timezone
-
-
-def get_sentinel_user(is_deleted=True):
-    if is_deleted:
-        username = 'deleted'
-    else:
-        username = 'unknown'
-    return auth.get_user_model().objects.get_or_create(username=username)[0]
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    # submitter = models.ForeignKey(auth.models.User, models.SET(
-    #     get_sentinel_user(is_deleted=True)))
+    submitter = models.ForeignKey(auth.models.User, null=False,
+                                  default=auth.models.User.objects.get_or_create(
+                                      username="unknown")[0].pk,
+                                  on_delete=models.SET(
+                                      auth.models.User.objects.get_or_create(
+                                          username="deleted")[0].pk))
 
     def was_published_recently(self):
         now = timezone.now()
