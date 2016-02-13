b4003ce371d7b431a9a6e874e3760fcebc415ffb
Ron <ronll64@gmail.com>
2016-02-13 14:23:07 +0200

    made some progress

--- polls/static/polls/javascript/add_question.js
+++ polls/static/polls/javascript/add_question.js
@@ -4,45 +4,55 @@ function getLastInput() {
 }
 
 function addOption() {
-		formTable  = $('#new_question_answers_table')[0];
+		if (!this.questionCount) {
+				this.questionCount = 1;
+		}
+
+		var formTable  = $('#new_question_answers_table')[0];
 
 		// One of the elements is the question text
-		numberOfInputs = formTable.getElementsByTagName('input').length - 1;
+		// var this.questionCount = formTable.getElementsByTagName('input').length - 1;
 
-		newOption = document.getElementById('id_question1').cloneNode(true);
+		var newOption = document.getElementById('id_question1').cloneNode(true);
 		newOption.getElementsByTagName('td')[0].innerText =
-				'Option ' + (numberOfInputs + 1).toString();
-		newOptionInput = newOption.getElementsByTagName('input')[0];
+				'Option ' + (this.questionCount + 1).toString();
+		var newOptionInput = newOption.getElementsByTagName('input')[0];
 		newOptionInput.type = 'text';
-		newOptionInput.name = 'option' + (numberOfInputs + 1).toString();
-		newOptionInput.id = "id_question" + (numberOfInputs + 1).toString();
+		newOptionInput.name = 'option' + (this.questionCount + 1).toString();
+		newOptionInput.id = "id_question" + (this.questionCount + 1).toString();
 
+		++this.questionCount;
 		formTable.appendChild(newOption);
 }
 
-
-function isNextOptionFilled(obj) {
+function handleInputChange(obj) {
 
 		if (obj.value) {
 				addOption();
 		}
 		else {
 				// We should remove the next option, if it is not empty
-				obj.parentElement().parentElement();
+				removeNextQuestionIfEmpty(obj);
 		}
 
 }
 
-function getNextQuestion(obj) {
+function getNextTr(obj) {
+		return parentTr.parent().children()[parentTr.index()+1];
+}
+
+function removeNextQuestionIfEmpty(obj) {
 		parentTr = $(obj.parentElement.parentElement);
 		if (parentTr.siblings().length == parentTr.index()) {
 				// Last element
 				return nil;
 		}
 		else {
-				nextTr = parentTr.parent().children()[parentTr.index()+1];
+ 				nextTr = parentTr.parent().children()[parentTr.index()+1];
 				if (!nextTr.value) {
 						nextTr.remove();
 				}
 		}
+
+		return nil;
 }
