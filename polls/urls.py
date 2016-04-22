0807bff0fe6b64e76bc1fe174b8a34cfcf32683c
Ron <ronll64@gmail.com>
2016-04-22 16:56:59 +0300

    added a test page for highcharts graph

--- polls/urls.py
+++ polls/urls.py
@@ -1,3 +1,4 @@
+from django.views.generic import TemplateView
 from django.conf.urls import url, include
 from django.contrib.auth.decorators import login_required
 
@@ -24,4 +25,5 @@ urlpatterns = [
     # url('^question', login_required(views.QuestionSubmit.as_view(
     #     template_name="polls/submit_question.djhtml")), name='add_question'),
     url('^question', login_required(views.addQuestion), name='add_question'),
+    url('^graph/', TemplateView.as_view(template_name='polls/graph.djhtml')),
 ]
